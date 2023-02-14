372345fe32e9b370bb7a11691749d017978b2418
Not <110069021+NOT39@users.noreply.github.com>
2023-02-14 19:30:45 -0300

    fix: delete api response substitute

--- src/hooks/useApi.ts
+++ src/hooks/useApi.ts
@@ -1,37 +1,26 @@
-import axios from 'axios'
+import axios from "axios"
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_REACT_APP_API_URL
+	baseURL: import.meta.env.VITE_REACT_APP_API_URL,
 })
 
-const validateToken = async(token: string) => {
-  return {
-    user: { id: 3, name: 'José', email: 'jose@email.com' }
-  }
-
-  const response = await api.post('/validate', { token })
-  return response.data
+const validateToken = async (token: string) => {
+	const response = await api.post("/validate", { token })
+	return response.data
 }
 
-const signin = async(email: string, password: string) => {
-  return {
-    user: { id: 3, name: 'José', email: 'jose@email.com' },
-    token: '123456789'
-  }
-  
-  const response = await api.post('/signin', {email, password})
-  return response.data
+const signin = async (email: string, password: string) => {
+	const response = await api.post("/signin", { email, password })
+	return response.data
 }
 
 const logout = async () => {
-  return { status: true }
-
-  const response = await api.post('/logout')
-  return response.data
+	const response = await api.post("/logout")
+	return response.data
 }
 
 export const useApi = () => ({
-  validateToken,
-  signin,
-  logout
+	validateToken,
+	signin,
+	logout,
 })
